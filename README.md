aaa88b424ee2fd41416210b55eb1cc14c0b48764
Raimon <raimon.txollo@gmail.com>
2025-05-05 13:15:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Star Wars Battles - Frontend
---
+++ src/main.tsx
@@ -0,0 +1,6 @@
+import { createRoot } from "react-dom/client";
+import "./styles/styles.css";
+
+createRoot(document.getElementById("root")!).render(
+  <div className="container" />
+);
