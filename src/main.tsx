46c33a7f17878cbdb6de5c4b6fad041d3c3e6af2
Raimon <raimon.txollo@gmail.com>
2025-05-07 17:01:36 +0200

    Add App and Layout component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
 import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import App from "./components/App/App";
+import "@fontsource-variable/roboto/index.css";
+import "@fontsource-variable/roboto-slab/index.css";
+import "@fontsource-variable/museomoderno/index.css";
 import "./styles/styles.css";
 
 createRoot(document.getElementById("root")!).render(
-  <div className="container" />
+  <StrictMode>
+    <App />
+  </StrictMode>,
 );
