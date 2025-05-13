b9a769559869e506e156f24fa53b2a08e5f937fb
Raimon <raimon.txollo@gmail.com>
2025-05-13 19:54:57 +0200

    Add Loader component, styles and test

---
+++ src/components/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import "./Loader.css";
+
+const Loader: React.FC = () => {
+  return (
+    <div className="loader">
+      <span>Battling the load...</span>
+    </div>
+  );
+};
+
+export default Loader;
