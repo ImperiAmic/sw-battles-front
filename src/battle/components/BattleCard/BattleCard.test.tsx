6d534413e9c2cff776a1714340a49ff4bda07062
Raimon <raimon.txollo@gmail.com>
2025-05-13 12:08:42 +0200

    Add tests for BattleCard and BattleList components

---
+++ src/battle/components/BattleCard/BattleCard.test.tsx
@@ -0,0 +1,60 @@
+import { Provider } from "react-redux";
+import { MemoryRouter } from "react-router";
+import { render, screen } from "@testing-library/react";
+import BattleCard from "./BattleCard";
+import { store } from "../../../store/store";
+import { battleOfMuret } from "../../../fixtures";
+
+describe("Given the BattleCard component", () => {
+  describe("When it receives Muret battle and renders", () => {
+    test("Then it should show 'Battle of Muret' as a heading", () => {
+      const expectedBattleName = /battle of muret/i;
+
+      render(
+        <Provider store={store}>
+          <MemoryRouter>
+            <BattleCard battle={battleOfMuret} />
+          </MemoryRouter>
+        </Provider>,
+      );
+
+      const battleTitle = screen.getByRole("heading", {
+        name: expectedBattleName,
+      });
+
+      expect(battleTitle).toBeInTheDocument();
+    });
+
+    test("Then it should show an illustration of the Battle of Muret", () => {
+      const expectedImageAltText = /illustration of the battle of muret/i;
+
+      render(
+        <Provider store={store}>
+          <MemoryRouter>
+            <BattleCard battle={battleOfMuret} />
+          </MemoryRouter>
+        </Provider>,
+      );
+
+      const battleImage = screen.getByAltText(expectedImageAltText);
+
+      expect(battleImage).toBeInTheDocument();
+    });
+
+    test("Then it should show the Empire icon", () => {
+      const expectedWinnerIconAltText = "Empire icon";
+
+      render(
+        <Provider store={store}>
+          <MemoryRouter>
+            <BattleCard battle={battleOfMuret} />
+          </MemoryRouter>
+        </Provider>,
+      );
+
+      const battleWinnerIcon = screen.getByAltText(expectedWinnerIconAltText);
+
+      expect(battleWinnerIcon).toBeInTheDocument();
+    });
+  });
+});
