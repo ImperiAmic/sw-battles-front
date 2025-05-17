03985ed1dc35e252f22339ef27d2892f9cbdea89
Raimon <raimon.txollo@gmail.com>
2025-05-17 11:20:21 +0200

    Fix Loader component

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,7 +1,21 @@
 import "./Loader.css";
 
 const Loader: React.FC = () => {
-  return <div className="loader">Battling the load...</div>;
+  return (
+    <div className="loader">
+      <span className="loader__main-text">LOADING...</span>
+      <span className="loader__secundary-text">
+        (or as Chebwacca would say: RRRRRRWWWWWGGHHHHH!)
+      </span>
+      <img
+        className="loader__image"
+        src="images/chewbacca.svg"
+        alt="Bust of Chewbacca"
+        height={240}
+        width={240}
+      />
+    </div>
+  );
 };
 
 export default Loader;
