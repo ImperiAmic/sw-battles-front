f2bc540aa5c2493a0fb773c2cd71a48ec225c016
Raimon <raimon.txollo@gmail.com>
2025-05-25 16:05:56 +0200

    Fix addBattles from useBattle hook

--- src/battle/hooks/types.ts
+++ src/battle/hooks/types.ts
@@ -1,4 +1,4 @@
-import type { BattleFormData, BattlesInfo } from "../../types";
+import type { BattleFormDataDto, BattlesInfo } from "../../types";
 
 export interface UseBattlesStructure {
   battlesInfo: BattlesInfo;
@@ -6,5 +6,5 @@ export interface UseBattlesStructure {
   toggleBattleWinner: (battleId: string) => Promise<void>;
   deleteBattle: (battleId: string) => Promise<void>;
   getBattleDetail: (battleId: string) => Promise<void>;
-  addBattle: (battleFormData: BattleFormData) => Promise<void>;
+  addBattle: (battleFormDataDto: BattleFormDataDto) => Promise<void>;
 }
